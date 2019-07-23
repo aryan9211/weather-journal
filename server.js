dbe99e1d1abcc3ad30cfa426617f3085045f5050
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-07-23 20:32:40 +0530

    install dependencies and add config

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// Require Express to run server and routes
+const app = express();
+const PORT = 3000;
 
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+app.use(cors({ origin: "*" }));
 
-// Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
-
-// Setup Server
+app.listen(PORT, () => console.log(`Application running on port: ${PORT}`));
