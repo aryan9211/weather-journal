508e0a8ebcd5da9aefdeded9beddffe01aac1347
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-07-24 09:10:27 +0530

    display entries in DOM

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,10 @@ const country = "in";
 const API_KEY = "fc27ee8b856655ef995c9dc03e29d192";
 const weatherUri = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 
+function formatDate(date) {
+  return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+}
+
 function resolveStatus(response) {
   if (response.status !== 200) throw new Error(response);
   return response.json();
@@ -10,6 +14,20 @@ function resolveStatus(response) {
 
 function updateJournalDOM(data) {
   console.log(data);
+  const entries = document.getElementById("entries");
+  if (!data.length) return;
+  data.forEach((item, index) => {
+    if (document.getElementById(`entry${index}`)) return;
+    const row = document.createElement("div");
+    row.id = `entry${index}`;
+    row.className = "table-row";
+    row.innerHTML = `
+      <p class="table-cell">${formatDate(new Date(item.date))}</p>
+      <p class="table-cell">${item.temp.toFixed(1)}</p>
+      <p class="table-cell">${item.userInput}</p>
+    `;
+    entries.appendChild(row);
+  });
 }
 
 function fetchJournalData() {
